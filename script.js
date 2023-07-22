77da7f964d1ec51313685401e3aebcc15b8b6247
Prabhunath <prabhunathshah2002@gmail.com>
2023-07-23 00:03:28 +0530

    New Changes

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+window.addEventListener("load", () => {
+  const loadingScreen = document.getElementById("loading-screen");
+  loadingScreen.style.display = "none";
+});
+
 const scroll = new LocomotiveScroll({
   el: document.querySelector("#main"),
   smooth: true,
@@ -43,15 +48,26 @@ function toggleOverlay() {
 // Call the toggleOverlay function when the page loads and when the window is resized
 window.addEventListener("load", toggleOverlay);
 window.addEventListener("resize", toggleOverlay);
+// Helper function to check if an element is visible within the viewport
+function isElementInViewport(el) {
+  const rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
 
 window.addEventListener("DOMContentLoaded", () => {
   const videos = document.querySelectorAll(".video");
-  let isVideoHovered = false;
   let currentVideo = null;
 
   videos.forEach((video) => {
     video.addEventListener("loadeddata", () => {
-      video.play();
+      if (!isSmallScreen) {
+        video.play(); // Autoplay videos on larger screens
+      }
       video.addEventListener("ended", () => {
         video.currentTime = 0;
         video.play();
@@ -60,15 +76,14 @@ window.addEventListener("DOMContentLoaded", () => {
     });
 
     video.addEventListener("mouseenter", () => {
-      isVideoHovered = true;
       currentVideo = video;
       pauseVideosExcept(currentVideo);
       video.currentTime = 0; // Start the hovered video from the beginning
       video.muted = false; // Unmute the hovered video
+      video.play(); // Play the video when hovered
     });
 
     video.addEventListener("mouseleave", () => {
-      isVideoHovered = false;
       currentVideo = null;
       playVideos();
       video.muted = true; // Mute all videos when not hovered
@@ -85,9 +100,27 @@ window.addEventListener("DOMContentLoaded", () => {
 
   function playVideos() {
     videos.forEach((video) => {
-      if (!isVideoHovered || video === currentVideo) {
+      if (isElementInViewport(video)) {
         video.play();
+      } else {
+        video.pause();
       }
     });
   }
+
+  // Use Intersection Observer to handle video visibility
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      const video = entry.target;
+      if (entry.isIntersecting) {
+        video.play();
+      } else {
+        video.pause();
+      }
+    });
+  });
+
+  videos.forEach((video) => {
+    observer.observe(video);
+  });
 });
